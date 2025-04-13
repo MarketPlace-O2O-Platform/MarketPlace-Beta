937b82fc3b9bb2760cc070c567fe22951873f825
82everywin <82everywin@gmail.com>
2025-04-14 03:17:33 +0900

    fix: 로그인페이지에서 팝업 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import CouponListPage from "./pages/CouponPage";
-import PopupControl from "./components/PopupControl.tsx";
 
 const App: React.FC = () => {
     return (
         <Router>
-            <PopupControl/>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/coupons" element={<CouponListPage />} />
